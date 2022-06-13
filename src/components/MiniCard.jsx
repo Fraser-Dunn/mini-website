9604a27c4cf2a73f1b9a0dd252f8bca652539349
Fraser-Dunn <fraseradunn@gmail.com>
2022-06-13 12:26:41 +0100

    Mini Cards somewhat styled

--- src/components/MiniCard.jsx
+++ src/components/MiniCard.jsx
@@ -1,16 +1,24 @@
 const MiniCard = (props) => {
   return (
     <>
-      <div className="items-center text-center w-[240px] h-[260px] outline outline-black px-2 py-2">
+      <div className="items-center text-center w-[240px] h-[260px] rounded-[2px] px-2 py-2 shadow-lg">
         <div>
-          <h1>{props.mini.name}</h1>
-          <p>{props.mini.size}</p>
+          <h1 className="bg-orange-400 text-lg font-semibold rounded-sm text-white hover:cursor-pointer">
+            {props.mini.name}
+          </h1>
+          <p className=" py-1 italic">{props.mini.size}</p>
         </div>
         <div className=" flex justify-center">
-          <img className="w-40" src={props.mini.imageUrls[0]} alt="/" />
+          <img
+            className="w-40 hover:cursor-pointer"
+            src={props.mini.imageUrls[0]}
+            alt="/"
+          />
         </div>
         <div>
-          <p>{props.mini.set}</p>
+          <p className="hover:underline hover:cursor-pointer pt-1 font-semibold">
+            {props.mini.set}
+          </p>
         </div>
       </div>
     </>
--- src/components/MiniGrid.jsx
+++ src/components/MiniGrid.jsx
@@ -4,7 +4,7 @@ import strahd from "../assets/strahd.json";
 const MiniGrid = (props) => {
   return (
     <div className="py-24 px-12">
-      <div className="grid-container">
+      <div className="grid-container ">
         {props.displayList.map((miniItem) => (
           <MiniCard key={miniItem.id} mini={miniItem} />
         ))}
