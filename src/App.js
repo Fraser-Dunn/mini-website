e8c2c2513d00f5da128c58fc3b70491e82983c81
Fraser-Dunn <fraseradunn@gmail.com>
2022-07-05 16:46:38 +0100

    About Page initialized

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import "react-toastify/dist/ReactToastify.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import About from "./pages/About";
 import Admin from "./pages/Admin";
 import LogIn from "./pages/LogIn";
 import MiniInfo from "./pages/MiniInfo";
@@ -19,6 +20,7 @@ function App() {
         <Navbar isAuthed={isAuthed} />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
           <Route path="/gallery" element={<Gallery />} />
           <Route path="/admin" element={<PrivateRoute isAuthed={isAuthed} />}>
             <Route path="/admin" element={<Admin />} />
