3e0f364f4df45db2317b2ace87aa1b9393f69dde
Fraser-Dunn <fraseradunn@gmail.com>
2022-07-20 23:21:53 +0100

    filter fixed

--- src/components/MiniGrid.jsx
+++ src/components/MiniGrid.jsx
@@ -1,16 +1,25 @@
 import MiniCard from "./MiniCard";
 
 const MiniGrid = ({ displayList, filtersList }) => {
+  const sameKeyFilters = {};
+  filtersList.forEach((filter) => {
+    if (!sameKeyFilters[filter.key]) {
+      sameKeyFilters[filter.key] = [];
+    }
+    sameKeyFilters[filter.key].push(filter.value);
+  });
+
   return (
     <div className="miniGrid-body">
       <div className="miniGrid-grid-container">
         {displayList.map((miniItem) => {
           let isRendered = true;
-          filtersList.forEach((filter) => {
-            if (miniItem[filter.key] !== filter.value) {
+          Object.entries(sameKeyFilters).forEach(([key, value]) => {
+            if (!value.includes(miniItem[key])) {
               isRendered = false;
             }
           });
+
           return isRendered ? (
             <MiniCard key={miniItem.id} mini={miniItem} />
           ) : null;
